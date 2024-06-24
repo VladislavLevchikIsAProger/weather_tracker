2cac35b833d955de61e63bf9bbf32b3c2032dd6b
Vladislav Levchik Work <vlad.levchik.ares@icloud.com>
2024-06-24 16:09:06 +0300

    feat: add HomeServlet

---
+++ src/main/java/com/vladislavlevchik/servlet/HomeServlet.java
@@ -0,0 +1,18 @@
+package com.vladislavlevchik.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/templates/home.html").forward(req, resp);
+    }
+}
