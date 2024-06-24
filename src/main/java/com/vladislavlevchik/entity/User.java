14ab4f8d027c28310a97ed21b2edaf5f9ce9dc6f
Vladislav Levchik Work <vlad.levchik.ares@icloud.com>
2024-06-24 16:06:40 +0300

    feat: add User entity

---
+++ src/main/java/com/vladislavlevchik/entity/User.java
@@ -0,0 +1,26 @@
+package com.vladislavlevchik.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "login", unique = true, nullable = false)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
