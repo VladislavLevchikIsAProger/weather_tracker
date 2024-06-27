2d64997f9c9e9e5ac3b4c51ffca6a5882044c721
Vladislav Levchik Work <vlad.levchik.ares@icloud.com>
2024-06-27 15:56:22 +0300

    feat: add DeauthenticationServlet

---
+++ src/main/java/com/vladislavlevchik/servlet/DeauthenticationServlet.java
@@ -0,0 +1,27 @@
+package com.vladislavlevchik.servlet;
+
+import com.vladislavlevchik.service.AuthenticationService;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class DeauthenticationServlet extends HttpServlet {
+    private final AuthenticationService authenticationService = new AuthenticationService();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Cookie cookie = authenticationService.findSessionIdCookie(req.getCookies());
+
+        cookie.setMaxAge(0);
+        resp.addCookie(cookie);
+
+        authenticationService.deleteSession(cookie.getValue());
+
+        resp.sendRedirect("/sign-in");
+    }
+}
