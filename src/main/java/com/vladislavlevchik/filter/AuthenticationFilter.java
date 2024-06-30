c3cadc89d1e22656d864e106f10897176f7112df
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-06-30 18:02:50 +0300

    feat: add AuthenticationFilter

---
+++ src/main/java/com/vladislavlevchik/filter/AuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.vladislavlevchik.filter;
+
+import com.vladislavlevchik.exception.IncorrectPasswordException;
+import com.vladislavlevchik.exception.UserAlreadyExistException;
+import com.vladislavlevchik.exception.UserNotFoundByLoginException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/sign-in")
+public class AuthenticationFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        try {
+            super.doFilter(req, res, chain);
+        } catch (IncorrectPasswordException | UserNotFoundByLoginException e) {
+            res.sendRedirect("/sign-in?error=true");
+        }
+    }
+}
