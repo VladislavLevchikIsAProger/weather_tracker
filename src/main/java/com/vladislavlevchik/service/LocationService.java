cd4c765f0481ed590bbe7f8b2fe9238d23200511
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-03 00:59:26 +0300

    feat: add getWeathers

--- src/main/java/com/vladislavlevchik/service/LocationService.java
+++ src/main/java/com/vladislavlevchik/service/LocationService.java
@@ -1,22 +1,40 @@
 package com.vladislavlevchik.service;
 
+import com.vladislavlevchik.dto.LocationRequestDto;
+import com.vladislavlevchik.dto.WeatherResponseDto;
 import com.vladislavlevchik.entity.Location;
 import com.vladislavlevchik.entity.User;
 import com.vladislavlevchik.repository.LocationRepository;
+import jakarta.servlet.ServletException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LocationService {
 
+    private final WeatherApiService weatherApiService = new WeatherApiService();
     private final LocationRepository locationRepository = new LocationRepository();
 
-    public void addLocation(User user, String name, Double lat, Double lon) {
+    public void addLocationToUser(User user, LocationRequestDto locationRequestDto) {
         Location location = Location.builder()
-                .name(name)
-                .lat(lat)
-                .lon(lon)
+                .name(locationRequestDto.getName())
+                .lat(locationRequestDto.getLat())
+                .lon(locationRequestDto.getLon())
                 .user(user)
                 .build();
 
         locationRepository.save(location);
+    }
+
+    public List<WeatherResponseDto> getWeathers(List<Location> locations) throws ServletException, IOException {
+        List<WeatherResponseDto> list = new ArrayList<>();
+
+        for(Location location: locations){
+            list.add(weatherApiService.getWeatherInfo(location));
+        }
+
+        return list;
     }
 
 }
