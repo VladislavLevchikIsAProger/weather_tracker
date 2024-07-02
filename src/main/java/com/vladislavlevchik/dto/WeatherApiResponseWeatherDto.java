9bf0a812953be0b8459c059ad638925784ca868f
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-03 01:00:29 +0300

    feat: add WeatherApiResponseWeatherDto

--- src/main/java/com/vladislavlevchik/dto/WeatherApiResponseWeatherDto.java
+++ src/main/java/com/vladislavlevchik/dto/WeatherApiResponseWeatherDto.java
@@ -1,4 +1,41 @@
 package com.vladislavlevchik.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherApiResponseWeatherDto {
+
+    @JsonProperty("main")
+    private WeatherMainDto main;
+
+    @JsonProperty("weather")
+    private WeatherDto[] weather;
+
+    @JsonProperty("wind")
+    private WindDto wind;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class WeatherMainDto {
+        private double temp;
+        @JsonProperty("feels_like")
+        private double feelsLike;
+        private int pressure;
+        private int humidity;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class WeatherDto {
+        private String description;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class WindDto {
+        private int speed;
+    }
 }
