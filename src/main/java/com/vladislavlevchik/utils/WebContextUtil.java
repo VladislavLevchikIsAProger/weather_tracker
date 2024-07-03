2f6e94aec7a03c0e20edf4d969468fc9b622b7a8
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-03 14:07:56 +0300

    feat: add WebContextUtil

---
+++ src/main/java/com/vladislavlevchik/utils/WebContextUtil.java
@@ -0,0 +1,22 @@
+package com.vladislavlevchik.utils;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.experimental.UtilityClass;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.IServletWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+@UtilityClass
+public class WebContextUtil {
+
+    public static WebContext buildWebContext(HttpServletRequest req, HttpServletResponse resp, ServletContext servletContext) {
+        JakartaServletWebApplication application = JakartaServletWebApplication.buildApplication(servletContext);
+        IServletWebExchange webExchange = application.buildExchange(req, resp);
+        return new WebContext(webExchange);
+    }
+
+}
