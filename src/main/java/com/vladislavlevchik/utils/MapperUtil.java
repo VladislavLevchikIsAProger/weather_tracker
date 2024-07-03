29daeef354b672d09e0444629844a135ec981c7e
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-03 14:10:42 +0300

    refactor: delete convertToDto

--- src/main/java/com/vladislavlevchik/utils/MapperUtil.java
+++ src/main/java/com/vladislavlevchik/utils/MapperUtil.java
@@ -19,15 +19,4 @@ public class MapperUtil {
     public static User convertToEntity(UserRequestDto userRequestDto) {
         return MODEL_MAPPER.map(userRequestDto, User.class);
     }
-
-    public static WeatherResponseDto convertToDto(WeatherApiResponseWeatherDto weatherApiResponseWeatherDto) {
-        return WeatherResponseDto.builder()
-                .temp((int) Math.round(weatherApiResponseWeatherDto.getMain().getTemp() - 273.15))
-                .feelsLike((int) Math.round(weatherApiResponseWeatherDto.getMain().getFeelsLike() - 273.15))
-                .description(weatherApiResponseWeatherDto.getWeather()[0].getDescription())
-                .humidity(weatherApiResponseWeatherDto.getMain().getHumidity())
-                .pressure(weatherApiResponseWeatherDto.getMain().getPressure())
-                .speed(weatherApiResponseWeatherDto.getWind().getSpeed())
-                .build();
-    }
 }
