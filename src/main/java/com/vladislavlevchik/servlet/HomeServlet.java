b1fbc6870e660a4450d9d2274b295beb1b478d68
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-04 14:44:55 +0300

    feat: add doPost

--- src/main/java/com/vladislavlevchik/servlet/HomeServlet.java
+++ src/main/java/com/vladislavlevchik/servlet/HomeServlet.java
@@ -1,5 +1,7 @@
 package com.vladislavlevchik.servlet;
 
+import com.vladislavlevchik.dto.CoordinatesRequestDto;
+import com.vladislavlevchik.dto.LocationRequestDto;
 import com.vladislavlevchik.dto.WeatherResponseDto;
 import com.vladislavlevchik.entity.Session;
 import jakarta.servlet.ServletException;
@@ -14,7 +16,7 @@ import java.util.List;
 public class HomeServlet extends WeatherTrackerBaseServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String sessionId = authenticationService.findSessionIdCookie(req.getCookies()).getValue();
 
         Session session = authenticationService.getSessionIfValid(sessionId);
@@ -22,7 +24,24 @@ public class HomeServlet extends WeatherTrackerBaseServlet {
         List<WeatherResponseDto> weathers = locationService.getWeathers(session.getUser().getLocations());
 
         context.setVariable("login", session.getUser().getLogin());
+        context.setVariable("weathers", weathers);
 
         templateEngine.process("home", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CoordinatesRequestDto coordinates = CoordinatesRequestDto.builder()
+                .lat(Double.parseDouble(req.getParameter("lat")))
+                .lon(Double.parseDouble(req.getParameter("lon")))
+                .build();
+
+        String sessionId = authenticationService.findSessionIdCookie(req.getCookies()).getValue();
+
+        Session session = authenticationService.getSessionIfValid(sessionId);
+
+        locationService.deleteLocationFromUser(coordinates, session.getUser());
+
+        resp.sendRedirect("/home");
     }
 }
