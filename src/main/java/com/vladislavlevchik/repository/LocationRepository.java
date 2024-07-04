e395a737e96627dce8f67171a1042e171ac9a42b
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-04 14:44:16 +0300

    feat: add delete

--- src/main/java/com/vladislavlevchik/repository/LocationRepository.java
+++ src/main/java/com/vladislavlevchik/repository/LocationRepository.java
@@ -2,6 +2,7 @@ package com.vladislavlevchik.repository;
 
 import com.vladislavlevchik.entity.Location;
 import com.vladislavlevchik.exception.DuplicateLocationException;
+import jakarta.persistence.Query;
 import org.hibernate.Session;
 import org.hibernate.exception.ConstraintViolationException;
 
@@ -18,6 +19,22 @@ public class LocationRepository extends BaseRepository {
             return entity;
         } catch (ConstraintViolationException e) {
             throw new DuplicateLocationException();
+        }
+    }
+
+    public void delete(Location location) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            String hql = "DELETE FROM Location l WHERE l.user = :user AND l.lat = :lat AND l.lon =:lon";
+            Query query = session.createQuery(hql);
+            query.setParameter("user", location.getUser());
+            query.setParameter("lat", location.getLat());
+            query.setParameter("lon", location.getLon());
+
+            query.executeUpdate();
+
+            session.getTransaction().commit();
         }
     }
 
